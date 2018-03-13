797c1333eec0b6f44a0c361396942cdcd204fd7f
Cristobal Sanchez <cristobal.sanchez@globant.com>
2018-03-13 15:26:02 -0300

    DF-272 Corrige URI de MongoDB y método de controlador.

--- src/main/java/cl/ido/ruta/controller/DummyController.java
+++ src/main/java/cl/ido/ruta/controller/DummyController.java
@@ -7,11 +7,9 @@ import cl.ido.ruta.service.DummyService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collections;
 import java.util.Map;
 
@@ -40,7 +38,7 @@ public class DummyController {
 
     @GetMapping(value = "/place/{name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @LoggingInfo
-    public Place findOneByName(String name) {
+    public Place findOneByName(@PathVariable(value = "name") String name) {
         return rutaRepository.findByName(name);
     }
 
